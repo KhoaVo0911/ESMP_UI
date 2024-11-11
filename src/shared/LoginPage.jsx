717b8c91f6366674d02876872d0e5b9dce7bbc5e
Võ Đăng Khoa <133868589+KhoaVo0911@users.noreply.github.com>
2024-11-11 15:17:43 +0700

    fix loginPage

--- src/shared/LoginPage.jsx
+++ src/shared/LoginPage.jsx
@@ -3,37 +3,38 @@ import { useNavigate } from "react-router-dom";
 import LoginComponent from "./Login";
 
 const LoginPage = () => {
-  const [accessToken, setAccessToken] = useState(""); // Store accessToken after login
-  const [vendorId, setVendorId] = useState(""); // Store vendorId after login
-  const [hostId, setHostId] = useState(""); // Store vendorId after login
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Check login status
-  const navigate = useNavigate(); // Initialize the navigate function from react-router-dom
+  const [accessToken, setAccessToken] = useState("");
+  const [vendorId, setVendorId] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
   const handleLoginSuccess = (token, userInfo) => {
     setAccessToken(token);
-    setVendorId(userInfo.vendorInfo.vendorId);
-    setIsLoggedIn(true);
-    setHostId(userInfo.hostInfo.hostId);
-
-    // Lưu accessToken, vendorName và urlQr vào sessionStorage
     sessionStorage.setItem("accessToken", token);
-    sessionStorage.setItem("vendorId", userInfo.vendorInfo.vendorId);
-    sessionStorage.setItem("vendorName", userInfo.vendorInfo.vendorName);
-    sessionStorage.setItem("urlQr", userInfo.vendorInfo.urlQr);
-    sessionStorage.setItem("hostId", userInfo.hostInfo.hostId);
 
-    const userRole = userInfo.role;
+    const hostCode = userInfo.hostInfo ? userInfo.hostInfo.hostCode : "";
+    setVendorId(hostCode);
 
-    // Điều hướng dựa trên vai trò của người dùng
+    setIsLoggedIn(true);
+
+    const userRole = userInfo.role || "host";
+
     if (userRole === "admin") {
-      navigate("/admin");
+      navigate("/admin", { state: { accessToken: token, vendorId: hostCode } });
     } else if (userRole === "manager") {
-      navigate("/DashboardVendor");
+      navigate("/DashboardVendor", {
+        state: { accessToken: token, vendorId: hostCode },
+      });
     } else if (userRole === "host") {
-      navigate("/dashboard");
+      const hostId = userInfo.hostInfo ? userInfo.hostInfo.hostId : "";
+      sessionStorage.setItem("hostId", hostId);
+
+      navigate("/dashboard", {
+        state: { accessToken: token, vendorId: hostCode, hostId: hostId },
+      });
     } else {
       console.error("Unknown role:", userRole);
-      navigate("/");
+      navigate("/", { state: { accessToken: token, vendorId: hostCode } });
     }
   };
 
